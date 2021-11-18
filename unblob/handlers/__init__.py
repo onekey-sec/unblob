e58019abe935b0e892a6b41283102872652e06d2
Quentin Kaiser <quentin@iot-inspector.com>
2021-11-18 13:32:49 +0100

    Working draft for squashfs chunk identification, carving, and extraction. Supports squashfs version 3 to 4 included. Supports little endian and big endian encodings.
    Add squashfs integreation tests.

--- unblob/handlers/__init__.py
+++ unblob/handlers/__init__.py
@@ -2,6 +2,7 @@ from typing import Dict, List
 
 from ..models import Handler
 from .archive import cab, cpio, tar, zip
+from .filesystem import squashfs
 
 
 def _make_handler_map(*handlers: Handler) -> Dict[str, Handler]:
@@ -12,6 +13,7 @@ _ALL_MODULES_BY_PRIORITY: List[Dict[str, Handler]] = [
     _make_handler_map(
         cab,
         zip,
+        squashfs,
         tar,
         cpio.PortableASCIIHandler,
         cpio.PortableASCIIWithCRCHandler,
