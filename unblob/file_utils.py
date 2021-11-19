c3af13ca58569560263f8325672a5c1a9d663daa
Kiss György <gyorgy@iot-inspector.com>
2021-11-19 16:44:16 +0100

    Add round_up helper function

--- unblob/file_utils.py
+++ unblob/file_utils.py
@@ -1,3 +1,11 @@
+import math
+
+
 def snull(content: bytes):
     """Strip null bytes from the end of the string."""
     return content.rstrip(b"\x00")
+
+
+def round_up(size: int, alignment: int):
+    """Round up size to the alignment boundary."""
+    return alignment * math.ceil(size / alignment)
