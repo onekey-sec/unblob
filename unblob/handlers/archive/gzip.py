e9d4c0fe5cc232a2f4246bc9ec81e441583868ac
Kiss György <gyorgy@iot-inspector.com>
2021-11-23 15:44:35 +0100

    Start using LimitedStartReader for every handler

--- unblob/handlers/archive/gzip.py
+++ unblob/handlers/archive/gzip.py
@@ -1,10 +1,10 @@
 import gzip
-import io
 import zlib
 from typing import List, Union
 
 from dissect.cstruct import cstruct
 
+from ...file_utils import LimitedStartReader
 from ...models import UnknownChunk, ValidChunk
 
 NAME = "gzip"
@@ -61,7 +61,7 @@ HEADER_STRUCT_SIZE = 10
 
 # FIXME: C901 '_calculate_end' is too complex
 def calculate_chunk(  # noqa: C901
-    file: io.BufferedReader, start_offset: int
+    file: LimitedStartReader, start_offset: int
 ) -> Union[UnknownChunk, ValidChunk]:  # type: ignore - not yet ready feature, will be overridden
     header = cparser.gzip_struct(file)
 
