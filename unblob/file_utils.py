6541bf15e3063603a5257c3f81a647856a5b1f55
Kiss György <gyorgy@iot-inspector.com>
2021-11-23 15:39:32 +0100

    Implement LimitedStartReader
    When we pass the open file to a Handler, sometimes we are using an external
    library and we don't have control over how they seek into the open file, so we
    need a wrapper which ensures that we never seek before the start of a Chunk.

--- unblob/file_utils.py
+++ unblob/file_utils.py
@@ -1,3 +1,4 @@
+import io
 import math
 
 
@@ -9,3 +10,23 @@ def snull(content: bytes):
 def round_up(size: int, alignment: int):
     """Round up size to the alignment boundary."""
     return alignment * math.ceil(size / alignment)
+
+
+class LimitedStartReader:
+    """Wrapper for open files, which
+    enforces that seekeng earlier than the start offset is not possible.
+    """
+
+    def __init__(self, file: io.BufferedIOBase, start: int):
+        self._file = file
+        self._start = start
+        self._file.seek(start)
+
+    def seek(self, offset: int, whence=io.SEEK_SET):
+        new_pos = self._file.seek(offset, whence)
+        if new_pos < self._start:
+            new_pos = self._file.seek(self._start, io.SEEK_SET)
+        return new_pos
+
+    def __getattr__(self, attr):
+        return getattr(self._file, attr)
