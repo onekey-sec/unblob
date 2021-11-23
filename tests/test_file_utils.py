09a7670a837ee891b4522c14b6d778e58b7fbfe7
Kiss György <gyorgy@iot-inspector.com>
2021-11-23 15:39:32 +0100

    Implement LimitedStartReader
    When we pass the open file to a Handler, sometimes we are using an external
    library and we don't have control over how they seek into the open file, so we
    need a wrapper which ensures that we never seek before the start of a Chunk.

--- tests/test_file_utils.py
+++ tests/test_file_utils.py
@@ -1,6 +1,8 @@
+import io
+
 import pytest
 
-from unblob.file_utils import round_up
+from unblob.file_utils import LimitedStartReader, round_up
 
 
 @pytest.mark.parametrize(
@@ -15,3 +17,32 @@ from unblob.file_utils import round_up
 )
 def test_round_up(size, alignment, result):
     assert round_up(size, alignment) == result
+
+
+@pytest.fixture()
+def fake_file() -> io.BytesIO:
+    return io.BytesIO(b"0123456789abcdefghijklmnopqrst")
+
+
+class TestLimitedStartReader:
+    def test_seek_forward(self, fake_file):
+        reader = LimitedStartReader(fake_file, 5)
+        reader.seek(10)
+        assert reader.tell() == 10
+
+    def test_seek_backward(self, fake_file):
+        reader = LimitedStartReader(fake_file, 5)
+        reader.seek(10)
+        reader.seek(-4, io.SEEK_CUR)
+        assert reader.tell() == 6
+
+    def test_seek_before_start(self, fake_file):
+        reader = LimitedStartReader(fake_file, 5)
+        reader.seek(10)
+        reader.seek(-6, io.SEEK_CUR)
+        assert reader.tell() == 5
+
+    def test_seek_to_end_of_file(self, fake_file):
+        reader = LimitedStartReader(fake_file, 5)
+        reader.seek(-1, io.SEEK_END)
+        assert reader.tell() == len(fake_file.getvalue()) - 1
