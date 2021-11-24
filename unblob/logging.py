0a195b2ff5ccedb073febe7c292c8d89da10bb4a
Kiss György <gyorgy@iot-inspector.com>
2021-11-24 14:54:00 +0100

    Format cstruct types with dumpstruct in log messages
    Similar to what we did with pathlib.Path objects, there is a pretty printer
    for cstruct objects too, so we can pretty print every parsed header to make
    debugging and development easier.

--- unblob/logging.py
+++ unblob/logging.py
@@ -2,22 +2,27 @@ import logging
 from pathlib import Path
 
 import structlog
+from dissect.cstruct import cstruct, dumpstruct
 
 
 def format_hex(value: int):
     return f"0x{value:x}"
 
 
-def pretty_print_paths(extract_root: Path):
-    def add_path(logger, method_name: str, event_dict: structlog.types.EventDict):
+def pretty_print_types(extract_root: Path):
+    def convert_type(logger, method_name: str, event_dict: structlog.types.EventDict):
         use_absolute_path = event_dict.pop("_absolute_path", False)
         for key, value in event_dict.items():
             if isinstance(value, Path):
                 path = value if use_absolute_path else value.relative_to(extract_root)
                 event_dict[key] = str(path)
+
+            elif isinstance(value, cstruct):
+                event_dict[key] = dumpstruct(value, output="string")
+
         return event_dict
 
-    return add_path
+    return convert_type
 
 
 class LazyField:
@@ -45,7 +50,7 @@ def configure_logger(verbose: bool, extract_root: Path):
         structlog.processors.TimeStamper(
             key="timestamp", fmt="%Y-%m-%d %H:%M.%S", utc=True
         ),
-        pretty_print_paths(extract_root),
+        pretty_print_types(extract_root),
         structlog.processors.UnicodeDecoder(),
         structlog.processors.StackInfoRenderer(),
         structlog.processors.format_exc_info,
