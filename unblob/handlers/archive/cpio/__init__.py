5dd1310b88825e0c95d49244ebb790da24bac6d9
Kiss György <gyorgy@iot-inspector.com>
2021-11-24 20:15:40 +0100

    Refactor CPIO handlers to the new class-based implementation
    The module structure have been simplified by moving them to the same module,
    and there is a new mixin class for the new ASCII based formats, which
    contains the elements which are the same for both.

--- unblob/handlers/archive/cpio/__init__.py
+++
@@ -1,4 +0,0 @@
-from .cpio_default import BinaryHandler
-from .cpio_portable_ascii import PortableASCIIHandler
-from .cpio_portable_ascii_crc import PortableASCIIWithCRCHandler
-from .cpio_portable_old_ascii import PortableOldASCIIHandler
