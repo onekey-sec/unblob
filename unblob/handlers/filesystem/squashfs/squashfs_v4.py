fb939cf15e202c3da2e31345afb0ed2b080d91ea
Kiss György <gyorgy@iot-inspector.com>
2021-11-24 10:14:09 +0100

    Use helper functions to simplify calculations

--- unblob/handlers/filesystem/squashfs/squashfs_v4.py
+++ unblob/handlers/filesystem/squashfs/squashfs_v4.py
@@ -3,6 +3,7 @@ from typing import List, Union
 
 from dissect.cstruct import cstruct
 
+from ....file_utils import round_up
 from ....models import UnknownChunk, ValidChunk
 
 NAME = "squashfs_v4"
@@ -56,8 +57,7 @@ def calculate_chunk(
     file: io.BufferedIOBase, start_offset: int
 ) -> Union[ValidChunk, UnknownChunk]:
     header = cparser.SQUASHFS4_SUPER_BLOCK(file)
-    # the actual size is padded to 4KiB
-    size = (1 + header.bytes_used // PAD_SIZE) * PAD_SIZE
+    size = round_up(header.bytes_used, PAD_SIZE)
     end_offset = start_offset + size
 
     return ValidChunk(start_offset=start_offset, end_offset=end_offset)
