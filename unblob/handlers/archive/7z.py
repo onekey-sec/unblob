aed4b5b731af8980c5116f6d3127380f52972381
Kiss György <gyorgy@iot-inspector.com>
2021-11-24 19:31:00 +0100

    Delete unfinished handlers
    We have merge request for better handlers for these.

--- unblob/handlers/archive/7z.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-from typing import List
-
-
-def make_extract_command(inpath: Path, outdir: Path) -> List[str]:
-    assert outdir.is_dir(), "Output path must be a directory for 7zip!"
-    infile = str(inpath.expanduser().resolve())
-    out = str(outdir.expanduser().resolve())
-    return ["7z", "x", "-y", infile, "-o", out]
