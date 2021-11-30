onekey-sec/unblob

commit e07e844abdc78198d017113c158ef7d6c68a4dca

Sort YARA matches by offset
From manual testing it looks like this is the default, but there is no
mention about this in the yara-python documentation

unblob/strategies.py
@@ -1,6 +1,6 @@
 import io
 import itertools
-from operator import attrgetter
+from operator import attrgetter, itemgetter
 from pathlib import Path
 from typing import Generator, List
 
@@ -31,7 +31,8 @@ def search_chunks_by_priority(
         for result in yara_results:
             handler = result.handler
             match = result.match
-            for offset, identifier, string_data in match.strings:
+            sorted_matches = sorted(match.strings, key=itemgetter(0), reverse=True)
+            for offset, identifier, string_data in sorted_matches:
                 logger.info(
                     "Calculating chunk for YARA match",
                     start_offset=format_hex(offset),

commit 865d629393622dd9241c3df9e7dfb380689758ed

Fix match sorting to ascending order
This should have never been in reverse order.
This bug was introduced in: e07e844abdc78198d017113c158ef7d6c68a4dca

unblob/strategies.py
@@ -31,7 +31,7 @@ def search_chunks_by_priority(  # noqa: C901
         for result in yara_results:
             handler = result.handler
             match = result.match
-            sorted_matches = sorted(match.strings, key=itemgetter(0), reverse=True)
+            sorted_matches = sorted(match.strings, key=itemgetter(0))
             for offset, identifier, string_data in sorted_matches:
                 logger.info(
                     "Calculating chunk for YARA match",