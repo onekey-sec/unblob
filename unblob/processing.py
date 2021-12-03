31fbf6efc04f701fd4b5d8611528f4eced09fbf0
György Kiss <gyorgy@iot-inspector.com>
2021-12-03 14:22:29 +0100

    Only stat processed files 1x instead of 3x
    All of the Path.is_dir(), Path.is_symlink() and similar stat-related methods
    will call stat syscall when they are called. We can spare extra syscalls by
    statting the file once and comparing the stat_result object.
    os.lstat() will not resolve symlinks, which the Path implementation uses and
    it is exactly what we want.

--- unblob/processing.py
+++ unblob/processing.py
@@ -1,3 +1,4 @@
+import stat
 from pathlib import Path
 
 from structlog import get_logger
@@ -23,21 +24,24 @@ def process_file(
 
     log.info("Start processing file")
 
-    if path.is_dir():
+    statres = path.lstat()
+    mode, size = statres.st_mode, statres.st_size
+
+    if stat.S_ISDIR(mode):
         log.info("Found directory")
         for path in path.iterdir():
             process_file(root, path, extract_root, max_depth, current_depth + 1)
         return
 
-    if path.is_symlink():
+    elif stat.S_ISLNK(mode):
         log.info("Ignoring symlink")
         return
 
-    file_size = path.stat().st_size
-    if file_size == 0:
+    elif size == 0:
         log.info("Ignoring empty file")
         return
 
-    log.info("Calculated file size", size=file_size)
-    for new_path in extract_with_priority(root, path, extract_root, file_size):
+    log.info("Calculated file size", size=size)
+
+    for new_path in extract_with_priority(root, path, extract_root, size):
         process_file(extract_root, new_path, extract_root, max_depth, current_depth + 1)
