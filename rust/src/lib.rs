cbc711ff0dfd14ef4a4acb28fd57f4a739df96bd
László Vaskó <laszlo@iot-inspector.com>
2021-12-13 11:51:59 +0100

    Rust: Shannon Entropy calculation
    Benchmark can be run with `cargo bench`

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,13 +1,16 @@
+pub(crate) mod math;
+
 use pyo3::prelude::*;
 
-/// Function defined in Rust
+/// Calculates Shannon entropy of data
 #[pyfunction(text_signature = "(data)")]
-pub fn hello_rust(name: &str) -> PyResult<String> {
-    Ok(format!("Hello from Rust to you, {}", name))
+pub fn shannon_entropy(data: &[u8]) -> PyResult<f64> {
+    Ok(math::shannon_entropy(data))
 }
 
+/// Performance sensitive functionality
 #[pymodule]
 fn _rust(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(hello_rust, m)?)?;
+    m.add_function(wrap_pyfunction!(shannon_entropy, m)?)?;
     Ok(())
 }
