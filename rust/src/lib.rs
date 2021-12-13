87f15701c69219146637e2f23f9bdeec5ede497e
László Vaskó <1771332+vlaci@users.noreply.github.com>
2021-12-13 08:42:31 +0100

    Rust: adding Python extension module with packaging
    As of now the extension packaging remains optional, unblob can be
    installed without any Rust tooling (skipping the build of the
    extension of course)
    
    The extension will load into the `unblob._rust` namespace in
    Python. Build uses the
    [PyLimitedAPI](https://docs.python.org/3.8/c-api/stable.html)
    functionality of Python, so that it doesn't need to get built for each
    Python version. The generated binary will be forward compatible from
    Python 3.8 onwards.
    
    Packaging plugs into poetry's `build.py` feature. Unfortunately this
    seams to be an undocumented extensibility point, but this is the only
    way a native module can be built. It works by basically hooking into
    a generated `setup.py` so that the `kwargs` sent to
    `setuptools.setup()` can be overridden.

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,8 +1,13 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+use pyo3::prelude::*;
+
+/// Function defined in Rust
+#[pyfunction(text_signature = "(data)")]
+pub fn hello_rust(name: &str) -> PyResult<String> {
+    Ok(format!("Hello from Rust to you, {}", name))
+}
+
+#[pymodule]
+fn _rust(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(hello_rust, m)?)?;
+    Ok(())
 }
