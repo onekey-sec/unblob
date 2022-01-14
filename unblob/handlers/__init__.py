4db08d6c794b2d9ddacaae646ae1116568915f14
Quentin Kaiser <quentin@iot-inspector.com>
2022-01-14 11:31:12 +0100

    Add support for NTFS filesystem.
    C struct built from documentation and NTFS3 open source library.
    
    Extraction is performed by 7z. Test files were built using mkfs.ntfs on Linux.

--- unblob/handlers/__init__.py
+++ unblob/handlers/__init__.py
@@ -3,7 +3,7 @@ from typing import List, Tuple, Type
 from ..models import Handler
 from .archive import ar, arc, arj, cab, cpio, dmg, rar, sevenzip, stuffit, tar, zip
 from .compression import bzip2, lz4, lzh, lzip, lzma, lzo, xz
-from .filesystem import cramfs, extfs, fat, iso9660, jffs2, squashfs, ubi
+from .filesystem import cramfs, extfs, fat, iso9660, jffs2, ntfs, squashfs, ubi
 
 ALL_HANDLERS_BY_PRIORITY: List[Tuple[Type[Handler], ...]] = [
     (
@@ -12,6 +12,7 @@ ALL_HANDLERS_BY_PRIORITY: List[Tuple[Type[Handler], ...]] = [
         fat.FATHandler,
         jffs2.JFFS2NewHandler,
         jffs2.JFFS2OldHandler,
+        ntfs.NTFSHandler,
         squashfs.SquashFSv3Handler,
         squashfs.SquashFSv4Handler,
         ubi.UBIHandler,
