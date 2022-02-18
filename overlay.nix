b2456ed0eb023e3996c7e7df0dfc35a4a56f05b1
László Vaskó <laszlo@iot-inspector.com>
2022-02-18 16:58:18 +0100

    nix: patch Squashfs-Tools
    Squashfs-Tools 9.2 introduced a regression which makes one of our
    tests to fail.

--- overlay.nix
+++ overlay.nix
@@ -4,4 +4,5 @@ final: prev:
   unblob = prev.callPackage ./. { };
   gnustep = prev.callPackage ./nix/gnustep { inherit (prev) gnustep; };
   yara = prev.callPackage ./nix/yara { inherit (prev) yara; };
+  squashfsTools = prev.callPackage ./nix/squashfsTools { inherit (prev) squashfsTools; };
 }
