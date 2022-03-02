c9930dc62aecb3069d413775764c6f48ae509868
László Vaskó <laszlo@iot-inspector.com>
2022-03-02 18:57:24 +0100

    nix: overriding the package name of sasquatch
    so it can be distinguished from `squashfsTools` and big/little endian
    variants can be told apart.

--- nix/sasquatch/default.nix
+++ nix/sasquatch/default.nix
@@ -2,10 +2,12 @@
 
 let
   self = squashfsTools.overrideAttrs (_: {
+    pname = "sasquatch";
     inherit src;
     patches = [ ]; # Patches are already applied to input
 
     passthru.bigEndian = self.overrideAttrs (super: {
+      pname = "${super.pname}-be";
       preConfigure = (super.preConfigure or "") + ''
         export CFLAGS="-DFIX_BE"
       '';
