7ce4d7c95a55cbc78666137c4c7e9196e8faa2a4
László Vaskó <laszlo@iot-inspector.com>
2022-03-02 18:52:16 +0100

    nix: add big-endian variant of sasquatch to unblob
    It can be built and tested via `nix build ".#sasquatch.bigEndian"`

--- nix/sasquatch/default.nix
+++ nix/sasquatch/default.nix
@@ -1,6 +1,18 @@
-{ src, squashfsTools }:
+{ src, lib, squashfsTools }:
 
-squashfsTools.overrideAttrs (super: {
-  inherit src;
-  patches = [ ]; # Patches are already applied to input
-})
+let
+  self = squashfsTools.overrideAttrs (_: {
+    inherit src;
+    patches = [ ]; # Patches are already applied to input
+
+    passthru.bigEndian = self.overrideAttrs (super: {
+      preConfigure = (super.preConfigure or "") + ''
+        export CFLAGS="-DFIX_BE"
+      '';
+      postInstall = (super.postInstall or "") + ''
+        mv $out/bin/sasquatch{,-v4be}
+      '';
+    });
+  });
+in
+self
