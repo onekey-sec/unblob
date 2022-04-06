700a20c103eae250e541d3c20c4ff4b1564e76da
Marton ILLES <marton.illes@iot-inspector.com>
2022-04-06 15:52:20 +0200

    add whitelist file to vulture

---
+++ vulture_whitelist.py
@@ -0,0 +1,16 @@
+import sys
+
+import unblob.plugins
+from unblob import cli
+from unblob.parser import _HexStringToRegex
+
+_HexStringToRegex.literal
+_HexStringToRegex.wildcard
+_HexStringToRegex.jump
+_HexStringToRegex.range_jump
+_HexStringToRegex.alternative
+
+sys.breakpointhook
+cli.cli.context_class
+
+unblob.plugins.hookimpl
