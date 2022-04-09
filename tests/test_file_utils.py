ce5da7bde0a60046fd6769aec56b39616fbd8225
Marton ILLES <marton.illes@iot-inspector.com>
2022-04-09 22:35:29 +0200

    drop LimitedStartReader

--- tests/test_file_utils.py
+++ tests/test_file_utils.py
@@ -1,7 +1,6 @@
 import io
 from pathlib import Path
 from typing import List
-from unittest.mock import MagicMock
 
 import pytest
 
@@ -9,7 +8,6 @@ from unblob.file_utils import (
     Endian,
     File,
     InvalidInputFormat,
-    LimitedStartReader,
     StructParser,
     convert_int8,
     convert_int16,
@@ -56,59 +54,6 @@ def test_round_up(size, alignment, result):
 @pytest.fixture()
 def fake_file() -> File:
     return File.from_bytes(b"0123456789abcdefghijklmnopqrst")
-
-
-class TestLimitedStartReader:
-    def test_seek_forward(self, fake_file):
-        reader = LimitedStartReader(fake_file, 5)
-        reader.seek(10)
-        assert reader.tell() == 10
-
-    def test_seek_backward(self, fake_file):
-        reader = LimitedStartReader(fake_file, 5)
-        reader.seek(10)
-        reader.seek(-4, io.SEEK_CUR)
-        assert reader.tell() == 6
-
-    def test_seek_before_start(self, fake_file):
-        reader = LimitedStartReader(fake_file, 5)
-        reader.seek(10)
-        reader.seek(-6, io.SEEK_CUR)
-        assert reader.tell() == 5
-
-    def test_seek_to_end_of_file(self, fake_file):
-        reader = LimitedStartReader(fake_file, 5)
-        reader.seek(-1, io.SEEK_END)
-        assert reader.tell() == len(fake_file.getvalue()) - 1
-
-    def test_seek_to_above_max_off_t(self, fake_file):
-        reader = LimitedStartReader(fake_file, 5)
-        with pytest.raises(InvalidInputFormat):
-            reader.seek(sys.maxsize + 1)
-
-    def test_seek_to_below_min_off_t(self, fake_file):
-        reader = LimitedStartReader(fake_file, 5)
-        with pytest.raises(InvalidInputFormat):
-            reader.seek(-(sys.maxsize + 2), io.SEEK_END)
-
-    @pytest.mark.parametrize(
-        "method_name",
-        ("detach", "read", "read1", "readinto", "readinto1"),
-    )
-    def test_methods_dispatched_to_file(self, method_name):
-        mock_file = MagicMock(io.BufferedReader)
-        reader = LimitedStartReader(mock_file, 10)
-
-        method = getattr(reader, method_name)
-        method("arg1", "arg2", kw1="kw1", kw2="kw2")
-
-        mock_method = getattr(mock_file, method_name)
-        mock_method.assert_called_with("arg1", "arg2", kw1="kw1", kw2="kw2")
-
-    def test_read_only(self, fake_file):
-        reader = LimitedStartReader(fake_file, 5)
-        with pytest.raises(TypeError):
-            reader.write(b"something")
 
 
 class TestStructParser:
--- unblob/file_utils.py
+++ unblob/file_utils.py
@@ -209,43 +209,6 @@ def iterate_file(
         yield data
 
 
-class LimitedStartReader(io.BufferedIOBase):
-    """Wrapper for open files, which
-    enforces that seekeng earlier than the start offset is not possible.
-    """
-
-    def __init__(self, file: io.BufferedIOBase, start: int):
-        self._file = file
-        self._start = start
-        self._file.seek(start)
-
-    def seek(self, offset: int, whence=io.SEEK_SET):
-        if (sys.maxsize < offset) or (offset < -(sys.maxsize + 1)):
-            raise InvalidInputFormat("File offset is greater than off_t (int64).")
-        new_pos = self._file.seek(offset, whence)
-        if new_pos < self._start:
-            new_pos = self._file.seek(self._start, io.SEEK_SET)
-        return new_pos
-
-    def write(self, *args, **kwargs):
-        raise TypeError("MUST NOT call write method")
-
-    def detach(self, *args, **kwargs):
-        return self._file.detach(*args, **kwargs)
-
-    def read(self, *args, **kwargs):
-        return self._file.read(*args, **kwargs)
-
-    def read1(self, *args, **kwargs):
-        return self._file.read1(*args, **kwargs)
-
-    def readinto(self, *args, **kwargs):
-        return self._file.readinto(*args, **kwargs)
-
-    def readinto1(self, *args, **kwargs):
-        return self._file.readinto1(*args, **kwargs)
-
-
 class StructParser:
     """Wrapper for dissect.cstruct to handle different endianness parsing dynamically."""
 
