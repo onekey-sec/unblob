a4d11dcc736af1ad9c146d458cedebd03eb2dcf9
Quentin Kaiser <quentin@iot-inspector.com>
2022-05-05 23:26:49 +0200

    Complete rewrite of XZ handler.
    In order to emit a single ValidChunk for concatenated XZ compression
    streams, we converted the handler to use Hyperscan like we did with
    bzip2 handler (see c97007735f985ecf3fbd0563f1102485dc2a5846).
    
    The idea is that for a given match, we use Hyperscan to look for end of
    stream markers after our current start offset. If we have a match, we
    read the whole chunk in memory and attempt to decompress it. If
    decompression is successful, we adjust start and end offsets of our
    context and instruct Hyperscan to continue looking for other end of
    stream marker matches. Otherwise, we stop the search and return.

---
+++ tests/handlers/compression/test_xz.py
@@ -0,0 +1,363 @@
+import pytest
+from helpers import unhex
+
+from unblob.file_utils import File, InvalidInputFormat
+from unblob.handlers.compression.xz import XZHandler
+
+XZ_NONE_CONTENT = unhex(
+    """\
+00000000  fd 37 7a 58 5a 00 00 00  ff 12 d9 41 02 00 21 01  |.7zXZ......A..!.|
+00000010  16 00 00 00 74 2f e5 a3  e0 00 8c 00 77 5d 00 2b  |....t/......w].+|
+00000020  9a 08 27 75 19 7c b8 ad  22 99 7e ea d4 e5 70 60  |..'u.|..".~...p`|
+00000030  bf 59 c1 89 bc 13 41 40  ca 24 c6 f9 ad 99 14 34  |.Y....A@.$.....4|
+00000040  d7 d6 85 be 0c ba a8 8b  a0 d7 97 72 da 7f e7 f2  |...........r....|
+00000050  73 78 69 3d cd b5 e6 45  2f b0 23 68 22 12 c8 e4  |................|
+00000060  43 f2 59 1b 17 3a ad 36  08 fb 21 c9 29 ba 13 1e  |C.Y..:.6..!.)...|
+00000070  cf c3 fb 69 65 57 5a 44  80 82 4d a1 a3 fd 8e 8d  |...ieWZD..M.....|
+00000080  f0 89 5f a6 71 c9 e8 44  80 cb f1 4e d0 cc 74 45  |.._.q..D...N..tE|
+00000090  fe 86 5b 63 00 00 00 00  00 01 8b 01 8d 01 00 00  |..[c............|
+000000a0  a7 1d b3 94 a8 00 0a fc  02 00 00 00 00 00 59 5a  |..............YZ|
+000000b0
+"""
+)
+
+XZ_NONE_CONTENT_BAD_IDX = unhex(
+    """\
+00000000  fd 37 7a 58 5a 00 00 00  ff 12 d9 41 02 00 21 01  |.7zXZ......A..!.|
+00000010  16 00 00 00 74 2f e5 a3  e0 00 8c 00 77 5d 00 2b  |....t/......w].+|
+00000020  9a 08 27 75 19 7c b8 ad  22 99 7e ea d4 e5 70 60  |..'u.|..".~...p`|
+00000030  bf 59 c1 89 bc 13 41 40  ca 24 c6 f9 ad 99 14 34  |.Y....A@.$.....4|
+00000040  d7 d6 85 be 0c ba a8 8b  a0 d7 97 72 da 7f e7 f2  |...........r....|
+00000050  73 78 69 3d cd b5 e6 45  2f b0 23 68 22 12 c8 e4  |................|
+00000060  43 f2 59 1b 17 3a ad 36  08 fb 21 c9 29 ba 13 1e  |C.Y..:.6..!.)...|
+00000070  cf c3 fb 69 65 57 5a 44  80 82 4d a1 a3 fd 8e 8d  |...ieWZD..M.....|
+00000080  f0 89 5f a6 71 c9 e8 44  80 cb f1 4e d0 cc 74 45  |.._.q..D...N..tE|
+00000090  fe 86 5b 63 00 00 00 00  01 01 8b 01 8d 01 00 00  |..[c............|
+000000a0  a7 1d b3 94 a8 00 0a fc  02 00 00 00 00 00 59 5a  |..............YZ|
+000000b0
+"""
+)
+
+XZ_CRC32_CONTENT = unhex(
+    """\
+00000000  fd 37 7a 58 5a 00 00 01  69 22 de 36 02 00 21 01  |.7zXZ...i".6..!.|
+00000010  16 00 00 00 74 2f e5 a3  e0 00 8c 00 77 5d 00 2b  |....t/......w].+|
+00000020  9a 08 27 75 19 7c b8 ad  22 99 7e ea d4 e5 70 60  |..'u.|..".~...p`|
+00000030  bf 59 c1 89 bc 13 41 40  ca 24 c6 f9 ad 99 14 34  |.Y....A@.$.....4|
+00000040  d7 d6 85 be 0c ba a8 8b  a0 d7 97 72 da 7f e7 f2  |...........r....|
+00000050  73 78 69 3d cd b5 e6 45  2f b0 23 68 22 12 c8 e4  |sxi=...E/..h"...|
+00000060  43 f2 59 1b 17 3a ad 36  08 fb 21 c9 29 ba 13 1e  |C.Y..:.6..!.)...|
+00000070  cf c3 fb 69 65 57 5a 44  80 82 4d a1 a3 fd 8e 8d  |...ieWZD..M.....|
+00000080  f0 89 5f a6 71 c9 e8 44  80 cb f1 4e d0 cc 74 45  |.._.q..D...N..tE|
+00000090  fe 86 5b 63 00 00 00 00  33 60 bb 0a 00 01 8f 01  |..[c....3`......|
+000000a0  8d 01 00 00 b1 5f 22 0f  3e 30 0d 8b 02 00 00 00  |....._".>0......|
+000000b0  00 01 59 5a                                       |..YZ|
+"""
+)
+
+XZ_CRC32_CONTENT_BAD_IDX = unhex(
+    """\
+00000000  fd 37 7a 58 5a 00 00 01  69 22 de 36 02 00 21 01  |.7zXZ...i".6..!.|
+00000010  16 00 00 00 74 2f e5 a3  e0 00 8c 00 77 5d 00 2b  |....t/......w].+|
+00000020  9a 08 27 75 19 7c b8 ad  22 99 7e ea d4 e5 70 60  |..'u.|..".~...p`|
+00000030  bf 59 c1 89 bc 13 41 40  ca 24 c6 f9 ad 99 14 34  |.Y....A@.$.....4|
+00000040  d7 d6 85 be 0c ba a8 8b  a0 d7 97 72 da 7f e7 f2  |...........r....|
+00000050  73 78 69 3d cd b5 e6 45  2f b0 23 68 22 12 c8 e4  |sxi=...E/..h"...|
+00000060  43 f2 59 1b 17 3a ad 36  08 fb 21 c9 29 ba 13 1e  |C.Y..:.6..!.)...|
+00000070  cf c3 fb 69 65 57 5a 44  80 82 4d a1 a3 fd 8e 8d  |...ieWZD..M.....|
+00000080  f0 89 5f a6 71 c9 e8 44  80 cb f1 4e d0 cc 74 45  |.._.q..D...N..tE|
+00000090  fe 86 5b 63 00 00 00 00  33 60 bb 0a 01 01 8f 01  |..[c....3`......|
+000000a0  8d 01 00 00 b1 5f 22 0f  3e 30 0d 8b 02 00 00 00  |....._".>0......|
+000000b0  00 01 59 5a                                       |..YZ|
+"""
+)
+
+XZ_CRC64_CONTENT = unhex(
+    """\
+00000000  fd 37 7a 58 5a 00 00 04  e6 d6 b4 46 02 00 21 01  |.7zXZ......F..!.|
+00000010  16 00 00 00 74 2f e5 a3  e0 00 8c 00 77 5d 00 2b  |....t/......w].+|
+00000020  9a 08 27 75 19 7c b8 ad  22 99 7e ea d4 e5 70 60  |..'u.|..".~...p`|
+00000030  bf 59 c1 89 bc 13 41 40  ca 24 c6 f9 ad 99 14 34  |.Y....A@.$.....4|
+00000040  d7 d6 85 be 0c ba a8 8b  a0 d7 97 72 da 7f e7 f2  |...........r....|
+00000050  73 78 69 3d cd b5 e6 45  2f b0 23 68 22 12 c8 e4  |sxi=...E/..h"...|
+00000060  43 f2 59 1b 17 3a ad 36  08 fb 21 c9 29 ba 13 1e  |C.Y..:.6..!.)...|
+00000070  cf c3 fb 69 65 57 5a 44  80 82 4d a1 a3 fd 8e 8d  |...ieWZD..M.....|
+00000080  f0 89 5f a6 71 c9 e8 44  80 cb f1 4e d0 cc 74 45  |.._.q..D...N..tE|
+00000090  fe 86 5b 63 00 00 00 00  55 8a 73 77 91 30 b0 a6  |..[c....U.sw.0..|
+000000a0  00 01 93 01 8d 01 00 00  51 9d 36 7b b1 c4 67 fb  |........Q.6{..g.|
+000000b0  02 00 00 00 00 04 59 5a                           |......YZ|
+"""
+)
+
+XZ_CRC64_CONTENT_BAD_IDX = unhex(
+    """\
+00000000  fd 37 7a 58 5a 00 00 04  e6 d6 b4 46 02 00 21 01  |.7zXZ......F..!.|
+00000010  16 00 00 00 74 2f e5 a3  e0 00 8c 00 77 5d 00 2b  |....t/......w].+|
+00000020  9a 08 27 75 19 7c b8 ad  22 99 7e ea d4 e5 70 60  |..'u.|..".~...p`|
+00000030  bf 59 c1 89 bc 13 41 40  ca 24 c6 f9 ad 99 14 34  |.Y....A@.$.....4|
+00000040  d7 d6 85 be 0c ba a8 8b  a0 d7 97 72 da 7f e7 f2  |...........r....|
+00000050  73 78 69 3d cd b5 e6 45  2f b0 23 68 22 12 c8 e4  |sxi=...E/..h"...|
+00000060  43 f2 59 1b 17 3a ad 36  08 fb 21 c9 29 ba 13 1e  |C.Y..:.6..!.)...|
+00000070  cf c3 fb 69 65 57 5a 44  80 82 4d a1 a3 fd 8e 8d  |...ieWZD..M.....|
+00000080  f0 89 5f a6 71 c9 e8 44  80 cb f1 4e d0 cc 74 45  |.._.q..D...N..tE|
+00000090  fe 86 5b 63 00 00 00 00  55 8a 73 77 91 30 b0 a6  |..[c....U.sw.0..|
+000000a0  01 01 93 01 8d 01 00 00  51 9d 36 7b b1 c4 67 fb  |........Q.6{..g.|
+000000b0  02 00 00 00 00 04 59 5a                           |......YZ|
+"""
+)
+
+XZ_SHA256_CONTENT = unhex(
+    """\
+00000000  fd 37 7a 58 5a 00 00 0a  e1 fb 0c a1 02 00 21 01  |.7zXZ.........!.|
+00000010  16 00 00 00 74 2f e5 a3  e0 00 8c 00 77 5d 00 2b  |....t/......w].+|
+00000020  9a 08 27 75 19 7c b8 ad  22 99 7e ea d4 e5 70 60  |..'u.|..".~...p`|
+00000030  bf 59 c1 89 bc 13 41 40  ca 24 c6 f9 ad 99 14 34  |.Y....A@.$.....4|
+00000040  d7 d6 85 be 0c ba a8 8b  a0 d7 97 72 da 7f e7 f2  |...........r....|
+00000050  73 78 69 3d cd b5 e6 45  2f b0 23 68 22 12 c8 e4  |sxi=...E/..h"...|
+00000060  43 f2 59 1b 17 3a ad 36  08 fb 21 c9 29 ba 13 1e  |C.Y..:.6..!.)...|
+00000070  cf c3 fb 69 65 57 5a 44  80 82 4d a1 a3 fd 8e 8d  |...ieWZD..M.....|
+00000080  f0 89 5f a6 71 c9 e8 44  80 cb f1 4e d0 cc 74 45  |.._.q..D...N..tE|
+00000090  fe 86 5b 63 00 00 00 00  8c e8 0b 54 41 0a 38 d9  |..[c.......TA.8.|
+000000a0  22 81 f0 55 47 bc ab 75  e3 a0 a8 0c 70 88 35 dd  |"..UG..u....p.5.|
+000000b0  c9 e5 44 4b 5f 81 7d 25  00 01 ab 01 8d 01 00 00  |..DK_.}%........|
+000000c0  91 18 1f 93 b6 e9 df 1c  02 00 00 00 00 0a 59 5a  |..............YZ|
+"""
+)
+
+XZ_SHA256_CONTENT_BAD_IDX = unhex(
+    """\
+00000000  fd 37 7a 58 5a 00 00 0a  e1 fb 0c a1 02 00 21 01  |.7zXZ.........!.|
+00000010  16 00 00 00 74 2f e5 a3  e0 00 8c 00 77 5d 00 2b  |....t/......w].+|
+00000020  9a 08 27 75 19 7c b8 ad  22 99 7e ea d4 e5 70 60  |..'u.|..".~...p`|
+00000030  bf 59 c1 89 bc 13 41 40  ca 24 c6 f9 ad 99 14 34  |.Y....A@.$.....4|
+00000040  d7 d6 85 be 0c ba a8 8b  a0 d7 97 72 da 7f e7 f2  |...........r....|
+00000050  73 78 69 3d cd b5 e6 45  2f b0 23 68 22 12 c8 e4  |sxi=...E/..h"...|
+00000060  43 f2 59 1b 17 3a ad 36  08 fb 21 c9 29 ba 13 1e  |C.Y..:.6..!.)...|
+00000070  cf c3 fb 69 65 57 5a 44  80 82 4d a1 a3 fd 8e 8d  |...ieWZD..M.....|
+00000080  f0 89 5f a6 71 c9 e8 44  80 cb f1 4e d0 cc 74 45  |.._.q..D...N..tE|
+00000090  fe 86 5b 63 00 00 00 00  8c e8 0b 54 41 0a 38 d9  |..[c.......TA.8.|
+000000a0  22 81 f0 55 47 bc ab 75  e3 a0 a8 0c 70 88 35 dd  |"..UG..u....p.5.|
+000000b0  c9 e5 44 4b 5f 81 7d 25  01 01 ab 01 8d 01 00 00  |..DK_.}%........|
+000000c0  91 18 1f 93 b6 e9 df 1c  02 00 00 00 00 0a 59 5a  |..............YZ|
+"""
+)
+
+FORMATS = (
+    pytest.param(
+        XZ_NONE_CONTENT,
+        id="none",
+    ),
+    pytest.param(
+        XZ_CRC32_CONTENT,
+        id="crc32",
+    ),
+    pytest.param(
+        XZ_CRC64_CONTENT,
+        id="crc64",
+    ),
+    pytest.param(
+        XZ_SHA256_CONTENT,
+        id="sha256",
+    ),
+)
+
+NULL_PADDING = b"\x00" * 4
+BAD_PADDING = b"\x00" * 3
+DATA_PADDING = b"123"
+
+
+@pytest.mark.parametrize(
+    "prefix",
+    (
+        pytest.param(
+            b"",
+            id="empty",
+        ),
+        pytest.param(
+            DATA_PADDING,
+            id="filled",
+        ),
+    ),
+)
+@pytest.mark.parametrize(
+    "suffix",
+    (
+        pytest.param(
+            b"",
+            id="empty",
+        ),
+        pytest.param(
+            DATA_PADDING,
+            id="filled",
+        ),
+    ),
+)
+@pytest.mark.parametrize(
+    "content, expected_length",
+    (
+        pytest.param(
+            XZ_NONE_CONTENT,
+            len(XZ_NONE_CONTENT),
+            id="crc_none",
+        ),
+        pytest.param(
+            XZ_CRC32_CONTENT,
+            len(XZ_CRC32_CONTENT),
+            id="crc32",
+        ),
+        pytest.param(
+            XZ_CRC64_CONTENT,
+            len(XZ_CRC64_CONTENT),
+            id="crc64",
+        ),
+        pytest.param(
+            XZ_SHA256_CONTENT,
+            len(XZ_SHA256_CONTENT),
+            id="crc_sha256",
+        ),
+        pytest.param(
+            XZ_NONE_CONTENT * 3,
+            len(XZ_NONE_CONTENT) * 3,
+            id="crc_none_concat",
+        ),
+        pytest.param(
+            XZ_CRC32_CONTENT * 3,
+            len(XZ_CRC32_CONTENT) * 3,
+            id="crc32_concat",
+        ),
+        pytest.param(
+            XZ_CRC64_CONTENT * 3,
+            len(XZ_CRC64_CONTENT) * 3,
+            id="crc64_concat",
+        ),
+        pytest.param(
+            XZ_SHA256_CONTENT * 3,
+            len(XZ_SHA256_CONTENT) * 3,
+            id="sha256_concat",
+        ),
+        pytest.param(
+            XZ_NONE_CONTENT + NULL_PADDING + XZ_NONE_CONTENT,
+            len(XZ_NONE_CONTENT + NULL_PADDING + XZ_NONE_CONTENT),
+            id="crc_none_concat_pad",
+        ),
+        pytest.param(
+            XZ_CRC32_CONTENT + NULL_PADDING + XZ_CRC32_CONTENT,
+            len(XZ_CRC32_CONTENT + NULL_PADDING + XZ_CRC32_CONTENT),
+            id="crc32_concat_pad",
+        ),
+        pytest.param(
+            XZ_CRC64_CONTENT + NULL_PADDING + XZ_CRC64_CONTENT,
+            len(XZ_CRC64_CONTENT + NULL_PADDING + XZ_CRC64_CONTENT),
+            id="concat_pad",
+        ),
+        pytest.param(
+            XZ_SHA256_CONTENT + NULL_PADDING + XZ_SHA256_CONTENT,
+            len(XZ_SHA256_CONTENT + NULL_PADDING + XZ_SHA256_CONTENT),
+            id="sha256_concat_pad",
+        ),
+        pytest.param(
+            XZ_NONE_CONTENT + NULL_PADDING + XZ_NONE_CONTENT + NULL_PADDING,
+            len(XZ_NONE_CONTENT + NULL_PADDING + XZ_NONE_CONTENT + NULL_PADDING),
+            id="crc_none_concat_pad2",
+        ),
+        pytest.param(
+            XZ_CRC32_CONTENT + NULL_PADDING + XZ_CRC32_CONTENT + NULL_PADDING,
+            len(XZ_CRC32_CONTENT + NULL_PADDING + XZ_CRC32_CONTENT + NULL_PADDING),
+            id="crc32_concat_pad2",
+        ),
+        pytest.param(
+            XZ_CRC64_CONTENT + NULL_PADDING + XZ_CRC64_CONTENT + NULL_PADDING,
+            len(XZ_CRC64_CONTENT + NULL_PADDING + XZ_CRC64_CONTENT + NULL_PADDING),
+            id="crc64_concat_pad2",
+        ),
+        pytest.param(
+            XZ_SHA256_CONTENT + NULL_PADDING + XZ_SHA256_CONTENT + NULL_PADDING,
+            len(XZ_SHA256_CONTENT + NULL_PADDING + XZ_SHA256_CONTENT + NULL_PADDING),
+            id="sha256_concat_pad2",
+        ),
+        pytest.param(
+            XZ_NONE_CONTENT + BAD_PADDING,
+            len(XZ_NONE_CONTENT),
+            id="crc_none_bad_padding",
+        ),
+        pytest.param(
+            XZ_CRC32_CONTENT + BAD_PADDING,
+            len(XZ_CRC32_CONTENT),
+            id="crc_crc32_bad_padding",
+        ),
+        pytest.param(
+            XZ_CRC64_CONTENT + BAD_PADDING,
+            len(XZ_CRC64_CONTENT),
+            id="crc_crc64_bad_padding",
+        ),
+        pytest.param(
+            XZ_SHA256_CONTENT + BAD_PADDING,
+            len(XZ_SHA256_CONTENT),
+            id="crc_sha256_bad_padding",
+        ),
+    ),
+)
+def test_xz_calculate_chunk(
+    prefix: bytes, content: bytes, expected_length: int, suffix: bytes
+):
+    start_offset = len(prefix)
+    handler = XZHandler()
+    fake_file = File.from_bytes(prefix + content + suffix)
+    fake_file.seek(start_offset)
+    chunk = handler.calculate_chunk(fake_file, start_offset)
+    assert chunk is not None
+    assert chunk.end_offset == start_offset + expected_length
+
+
+@pytest.mark.parametrize(
+    "padding",
+    (
+        pytest.param(
+            b"",
+            id="empty",
+        ),
+        pytest.param(
+            NULL_PADDING,
+            id="nullpad",
+        ),
+    ),
+)
+@pytest.mark.parametrize("first_chunk", FORMATS)
+@pytest.mark.parametrize("second_chunk", FORMATS)
+def test_xz_calculate_chunk_unique_stream(
+    first_chunk: bytes, second_chunk: bytes, padding: bytes
+):
+    """We should emit a chunk for XZ streams having the same flags."""
+    handler = XZHandler()
+    fake_file = File.from_bytes(first_chunk + padding + second_chunk)
+    chunk = handler.calculate_chunk(fake_file, 0)
+    assert chunk is not None
+    assert chunk.end_offset == len(first_chunk + padding + second_chunk)
+
+
+@pytest.mark.parametrize(
+    "content",
+    (
+        pytest.param(
+            XZ_NONE_CONTENT_BAD_IDX,
+            id="crc_none_bad_idx",
+        ),
+        pytest.param(
+            XZ_CRC32_CONTENT_BAD_IDX,
+            id="crc_crc32_bad_idx",
+        ),
+        pytest.param(
+            XZ_CRC64_CONTENT_BAD_IDX,
+            id="crc_crc64_bad_idx",
+        ),
+        pytest.param(
+            XZ_SHA256_CONTENT_BAD_IDX,
+            id="crc_sha256_bad_idx",
+        ),
+    ),
+)
+def test_xz_calculate_chunk_error(content: bytes):
+    handler = XZHandler()
+    fake_file = File.from_bytes(content)
+    with pytest.raises(InvalidInputFormat):
+        handler.calculate_chunk(fake_file, 0)
--- unblob/handlers/compression/xz.py
+++ unblob/handlers/compression/xz.py
@@ -1,6 +1,8 @@
 import io
 from typing import Optional, Tuple
 
+import attr
+import hyperscan
 from structlog import get_logger
 
 from unblob.extractors import Command
@@ -8,27 +10,73 @@ from unblob.extractors import Command
 from ...file_utils import (
     Endian,
     convert_int8,
+    convert_int16,
     convert_int32,
     decode_multibyte_integer,
-    iterate_patterns,
+    read_until_past,
     round_up,
 )
-from ...models import File, Handler, HexString, ValidChunk
+from ...models import File, Handler, HexString, InvalidInputFormat, ValidChunk
 
 logger = get_logger()
 
 # The .xz file format definition: https://tukaani.org/xz/xz-file-format-1.0.4.txt
 
-XZ_PADDING = 4  # XZ format byte alignment
-MAX_MBI_LEN = 9  # maximum multi-byte integer size is 9, per XZ standard
-STREAM_HEADER_SIZE = 12  # magic bytes (6) + flags (2) + CRC32 (4)
-STREAM_FOOTER_SIZE = 12  # CRC32 (4) + backward size (4) + flags (2) + magic bytes (2)
-STREAM_FOOTER_MAGIC = b"YZ"
+STREAM_START_MAGIC = b"\xFD\x37\x7A\x58\x5A\x00"
 
-MAGIC_BYTES_SIZE = 6
+STREAM_END_MAGIC_PATTERNS = [
+    HexString("00 00 59 5A"),  # None
+    HexString("00 01 59 5A"),  # CRC32
+    HexString("00 04 59 5A"),  # CRC64
+    HexString("00 0A 59 5A"),  # SHA-256
+]
+
+NONE_STREAM_FLAG = 0x0
+CRC32_STREAM_FLAG = 0x1
+CRC64_STREAM_FLAG = 0x4
+SHA256_STREAM_FLAG = 0xA
+VALID_FLAGS = [
+    NONE_STREAM_FLAG,
+    CRC32_STREAM_FLAG,
+    CRC64_STREAM_FLAG,
+    SHA256_STREAM_FLAG,
+]
 BACKWARD_SIZE_LEN = 4
-CRC32_LEN = 4
+MAX_MBI_LEN = 9  # maximum multi-byte integer size is 9, per XZ standard
+XZ_PADDING = 4  # XZ format byte alignment
 FLAG_LEN = 2
+EOS_MAGIC_LEN = 2
+CRC32_LEN = 4
+STREAM_HEADER_LEN = len(STREAM_START_MAGIC) + FLAG_LEN + CRC32_LEN
+STREAM_FOOTER_LEN = CRC32_LEN + BACKWARD_SIZE_LEN + FLAG_LEN + EOS_MAGIC_LEN
+
+
+def build_stream_end_scan_db(pattern_list):
+    patterns = []
+    for pattern_id, pattern in enumerate(pattern_list):
+        patterns.append(
+            (
+                pattern.as_regex(),
+                pattern_id,
+                hyperscan.HS_FLAG_SOM_LEFTMOST | hyperscan.HS_FLAG_DOTALL,
+            )
+        )
+
+    expressions, ids, flags = zip(*patterns)
+    db = hyperscan.Database(mode=hyperscan.HS_MODE_VECTORED)
+    db.compile(expressions=expressions, ids=ids, elements=len(patterns), flags=flags)
+    return db
+
+
+hyperscan_stream_end_magic_db = build_stream_end_scan_db(STREAM_END_MAGIC_PATTERNS)
+
+
+@attr.define
+class XZSearchContext:
+    start_offset: int
+    file: File
+    end_streams_offset: int
+    stream_flag: int
 
 
 def read_multibyte_int(file: File) -> Tuple[int, int]:
@@ -40,6 +88,83 @@ def read_multibyte_int(file: File) -> Tuple[int, int]:
     return size, mbi
 
 
+def get_stream_size(footer_offset: int, file: File) -> int:
+    file.seek(footer_offset - BACKWARD_SIZE_LEN, io.SEEK_SET)
+    backward_bytes = file.read(BACKWARD_SIZE_LEN)
+    stored_backward_size = convert_int32(backward_bytes, Endian.LITTLE)
+    real_backward_size = (stored_backward_size + 1) * 4
+
+    # skip backwards to the end of the Index
+    file.seek(-CRC32_LEN - BACKWARD_SIZE_LEN, io.SEEK_CUR)
+
+    # skip backwards of backward size to the start of index
+    file.seek(-real_backward_size, io.SEEK_CUR)
+
+    index_size = 0
+    index_indicator = convert_int8(file.read(1), Endian.LITTLE)
+    # index indicator must be 0, per xz standard
+    if index_indicator != 0:
+        raise InvalidInputFormat("Invalid index indicator")
+
+    index_size += 1
+
+    # read Index 'Number of Records'
+    size, num_records = read_multibyte_int(file)
+    index_size += size
+
+    # read Record 'Unpadded Size' and 'Uncompressed Size' for every Record
+    blocks_size = 0
+    for _ in range(0, num_records):
+        size, unpadded_size = read_multibyte_int(file)
+        index_size += size
+
+        size, _ = read_multibyte_int(file)
+        index_size += size
+
+        blocks_size += round_up(unpadded_size, XZ_PADDING)
+
+    index_size += CRC32_LEN
+
+    stream_size = round_up(
+        (STREAM_HEADER_LEN + blocks_size + index_size + STREAM_FOOTER_LEN),
+        XZ_PADDING,
+    )
+    return stream_size
+
+
+def _hyperscan_match(
+    pattern_id: int, offset: int, end: int, flags: int, context: XZSearchContext
+) -> bool:
+
+    # if we matched before our start offset, continue looking
+    end_offset = offset + FLAG_LEN + EOS_MAGIC_LEN
+    if end_offset < context.start_offset:
+        return False
+
+    stream_size = get_stream_size(offset, context.file)
+
+    if stream_size != (end_offset - context.start_offset):
+        return True
+
+    # stream padding validation
+    # padding MUST contain only null bytes and be 4 bytes aligned
+    context.file.seek(end_offset)
+    end_padding_offset = read_until_past(context.file, b"\x00")
+    padding_size = end_padding_offset - end_offset
+    if padding_size % 4 != 0:
+        context.end_streams_offset = end_offset
+        return True
+
+    # next magic validation
+    context.end_streams_offset = end_padding_offset
+    context.file.seek(end_padding_offset, io.SEEK_SET)
+    magic = context.file.read(len(STREAM_START_MAGIC))
+    if magic == STREAM_START_MAGIC:
+        context.start_offset = end_padding_offset
+        return False
+    return True
+
+
 class XZHandler(Handler):
     NAME = "xz"
 
@@ -49,62 +174,31 @@ class XZHandler(Handler):
 
     def calculate_chunk(self, file: File, start_offset: int) -> Optional[ValidChunk]:
 
-        file.seek(MAGIC_BYTES_SIZE, io.SEEK_CUR)
-        stream_flags = file.read(FLAG_LEN)
-        file.seek(start_offset)
+        file.seek(start_offset + len(STREAM_START_MAGIC), io.SEEK_SET)
+        stream_flag = convert_int16(file.read(2), Endian.BIG)
+        if stream_flag not in VALID_FLAGS:
+            raise InvalidInputFormat("Invalid stream flag for xz stream.")
 
-        for footer_offset in iterate_patterns(file, stream_flags + STREAM_FOOTER_MAGIC):
-            # make sure it's byte aligned
-            if (footer_offset - start_offset) % 4 != 0:
-                continue
+        context = XZSearchContext(
+            start_offset=start_offset,
+            file=file,
+            end_streams_offset=-1,
+            stream_flag=stream_flag,
+        )
 
-            end_offset = footer_offset + CRC32_LEN
-
-            file.seek(footer_offset - BACKWARD_SIZE_LEN)
-
-            backward_bytes = file.read(BACKWARD_SIZE_LEN)
-            stored_backward_size = convert_int32(backward_bytes, Endian.LITTLE)
-            real_backward_size = (stored_backward_size + 1) * 4
-
-            if real_backward_size > file.tell():
-                continue
-
-            file.seek(-CRC32_LEN - BACKWARD_SIZE_LEN, io.SEEK_CUR)
-            # skip backwards of backward size to the start of index
-            file.seek(-real_backward_size, io.SEEK_CUR)
-
-            index_size = 0
-            index_indicator = convert_int8(file.read(1), Endian.LITTLE)
-            # index indicator must be 0, per xz standard
-            if index_indicator != 0:
-                continue
-
-            index_size += 1
-
-            # read Index 'Number of Records'
-            size, num_records = read_multibyte_int(file)
-            index_size += size
-
-            # read Record 'Unpadded Size' and 'Uncompressed Size' for every Record
-            blocks_size = 0
-            for _ in range(0, num_records):
-                size, unpadded_size = read_multibyte_int(file)
-                index_size += size
-
-                size, _ = read_multibyte_int(file)
-                index_size += size
-
-                blocks_size += round_up(unpadded_size, XZ_PADDING)
-
-            index_size += CRC32_LEN
-
-            overall_size = round_up(
-                (STREAM_HEADER_SIZE + blocks_size + index_size + STREAM_FOOTER_SIZE),
-                XZ_PADDING,
+        try:
+            hyperscan_stream_end_magic_db.scan(
+                [file], match_event_handler=_hyperscan_match, context=context
+            )
+        except hyperscan.error as e:
+            if e.args and e.args[0] != f"error code {hyperscan.HS_SCAN_TERMINATED}":
+                logger.debug(
+                    "Error scanning for xz patterns",
+                    error=e,
+                )
+        if context.end_streams_offset > 0:
+            return ValidChunk(
+                start_offset=start_offset, end_offset=context.end_streams_offset
             )
 
-            # if the identified chunk size is equal to the size calculated from
-            # the Index's records, that means we matched on the right footer and
-            # we can return
-            if (end_offset - start_offset) == overall_size:
-                return ValidChunk(start_offset=start_offset, end_offset=end_offset)
+        raise InvalidInputFormat("No valid xz compression stream was detected")
