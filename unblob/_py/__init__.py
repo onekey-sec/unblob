091e69bf65e6e17f91bc3f5d7910dc71fdda3d8a
László Vaskó <laszlo.vasko@onekey.com>
2022-07-15 16:52:40 +0100

    mypyc

--- unblob/_py/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Python implementation of the algorithms found in the Rust extension module"""
-
-from .math import shannon_entropy
--- unblob/math.py
+++ unblob/math.py
@@ -1,4 +1,17 @@
-try:
-    from ._rust import shannon_entropy
-except ImportError:
-    from ._py.math import shannon_entropy  # noqa: F401
+import math
+
+
+def shannon_entropy(data: bytes) -> float:
+    counts = [0] * 256
+    for b in data:
+        counts[b] += 1
+
+    ent = 0.0
+
+    for c in counts:
+        if c == 0:
+            continue
+        p = c / len(data)
+        ent -= p * math.log(p, 2)
+
+    return ent
