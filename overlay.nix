ed982526f779708fff8221a89ffefae7d35f0167
László Vaskó <laszlo@iot-inspector.com>
2023-02-19 22:36:49 +0100

    Revert "nix: dramatically reduce closure size of `unar`"
    This reverts commit 01180ffaa6734c3c19fba85bf1b1af7d6a44ca6f.
    
    The issue has been fixed upstream

--- overlay.nix
+++ overlay.nix
@@ -2,7 +2,6 @@ inputs: final: prev:
 
 {
   unblob = prev.callPackage ./. { };
-  gnustep = prev.callPackage ./nix/gnustep { inherit (prev) gnustep; };
   sasquatch = prev.callPackage ./nix/sasquatch { inherit (prev) squashfsTools; src = inputs.sasquatch; };
   mkPoetryApp = prev.callPackage ./nix/poetry { };
 }
