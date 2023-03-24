16078eaa17e63bedf2d7a3718abd5e5776d7f00c
Quentin Kaiser <quentin.kaiser@onekey.com>
2023-03-24 14:49:34 +0100

    chore(deps): remove yaffshiv from Nix recipes.

--- nix/python/default.nix
+++ nix/python/default.nix
@@ -4,7 +4,6 @@ let
   self = python3.override {
     inherit self;
     packageOverrides = final: prev: {
-      yaffshiv = final.callPackage ../yaffshiv { };
       pyperscan = pyperscan.packages.${system}.default.vectorscan;
     };
   };
