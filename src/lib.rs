71a93bd1829f6d0a1c2e63f7eb6d30a4ff412b56
László Vaskó <1771332+vlaci@users.noreply.github.com>
2023-06-02 15:02:12 +0200

    refact: move python submodule initialization to rust submodule

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,11 @@
-pub mod math;
+pub mod math_tools;
 
 use pyo3::prelude::*;
-
-/// Calculates Shannon entropy of data
-#[pyfunction(text_signature = "(data)")]
-pub fn shannon_entropy(py: Python, data: &[u8]) -> PyResult<f64> {
-    py.allow_threads(|| Ok(math::shannon_entropy(data)))
-}
 
 /// Performance-critical functionality
 #[pymodule]
 fn _native(py: Python, m: &PyModule) -> PyResult<()> {
-    let math_module = PyModule::new(py, "math_tools")?;
-    math_module.add_function(wrap_pyfunction!(shannon_entropy, math_module)?)?;
+    math_tools::init_module(py, m)?;
 
-    m.add_submodule(math_module)?;
     Ok(())
 }
