137270c684023ccd8b9f6ef9c4c64bc3b6bb7849
László Vaskó <1771332+vlaci@users.noreply.github.com>
2023-06-07 11:24:56 +0200

    chore(nix): make `update-python-libraries` helper work
    Running e.g `update-python-libraries nix/treelib` would update the
    vendored treelib derivation to the latest version from pypi
    
    It was necessary to shadow nix's `python3` and `python3Packages`
    derivations to make this update script work

--- overlay.nix
+++ overlay.nix
@@ -1,7 +1,18 @@
 inputs: final: prev:
 
 {
-  unblob = final.unblobPython.pkgs.callPackage ./nix/unblob { };
-  unblobPython = prev.callPackage ./nix/python { inherit (inputs) unblob-native pyperscan; };
-  craneLib = inputs.crane.lib.${final.system};
+  inherit (final.python3.pkgs) unblob;
+  python3 = prev.python3 // {
+    pkgs = prev.python3.pkgs.overrideScope
+      (pyFinal: pyPrev: {
+        pyperscan = inputs.pyperscan.packages.${final.system}.default.vectorscan;
+        unblob-native = inputs.unblob-native.packages.${final.system}.default;
+        treelib = pyFinal.callPackage ./nix/treelib { };
+        lzallright = pyFinal.callPackage ./nix/lzallright { };
+        ubi_reader = pyFinal.callPackage ./nix/ubi_reader { };
+        jefferson = pyFinal.callPackage ./nix/jefferson { };
+        unblob = pyFinal.callPackage ./nix/unblob { };
+      });
+  };
+  python3Packages = final.python3.pkgs;
 }
