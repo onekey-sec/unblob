171e2677e5277eedf3a715dd9fdf88683e8144e4
László Vaskó <1771332+vlaci@users.noreply.github.com>
2023-09-04 14:35:34 +0200

    nix/pyfatfs: fix build

--- nix/pyfatfs/default.nix
+++ nix/pyfatfs/default.nix
@@ -3,8 +3,17 @@
 python3.pkgs.buildPythonApplication rec {
   inherit (_sources.pyfatfs) pname version src;
 
-  format = "pyproject";
+  format = "setuptools";
 
-  nativeBuildInputs = with python3.pkgs; [ setuptools setuptools-scm ];
+  doCheck = false;
+
+  nativeBuildInputs = with python3.pkgs; [ pytest-runner setuptools-scm ];
+
   propagatedBuildInputs = with python3.pkgs; [ fs ];
+
+  postPatch = ''
+    substituteInPlace ./setup.py --replace 'setuptools_scm~=5.0.0' setuptools_scm
+  '';
+
+  env.SETUPTOOLS_SCM_PRETEND_VERSION = version;
 }
