33aca36856a8ca1dbf7df4f59b8c9feb069ec0ca
Quentin Kaiser <quentin.kaiser@onekey.com>
2023-09-04 11:56:28 +0200

    fix(handlers): rely on pyfatfs to compute FAT images end offset
    Some FAT samples that we have are truncated to match exactly the size of
    reserved sectors + FAT sectors + active cluster sectors. It's like the
    initial image minus the free clusters.
    
    We switched our approach for end offset calculation by simply
    calculating the offset of the highest cluster within the filesystem. The
    end offset corresponds to the offset of that cluster + the size of the
    file located there.
    
    Parsing is performed by pyfatfs, which implements similar structure
    validation so whenever an error raised we yield an InvalidInputFormat.

--- unblob/handlers/filesystem/fat.py
+++ unblob/handlers/filesystem/fat.py
@@ -1,21 +1,39 @@
 from typing import Optional
 
-from dissect.cstruct import Instance
+from pyfatfs._exceptions import PyFATException
+from pyfatfs.PyFat import PyFat
 from structlog import get_logger
 
 from unblob.extractors.command import Command
 from unblob.file_utils import InvalidInputFormat
 
-from ...models import File, HexString, StructHandler, ValidChunk
+from ...models import File, Handler, HexString, ValidChunk
 
 logger = get_logger()
 
 
-VALID_NT_SIGNATURES = [0x28, 0x29]
-VALID_MEDIAS = [0xF0, 0xF8, 0xF9, 0xFA, 0xFB, 0xFC, 0xFD, 0xFE, 0xFF]
+def get_max_offset(
+    fs: PyFat, root_dir, max_cluster: int = 0, max_offset: int = 0
+) -> int:
+    dirs, files, _ = root_dir.get_entries()
+
+    for d in dirs:
+        if d.get_cluster() > max_cluster:
+            max_cluster = d.get_cluster()
+            max_offset = fs.get_data_cluster_address(max_cluster) + d.get_size()
+        max_offset = get_max_offset(fs, d, max_cluster, max_offset)
+    for f in files:
+        if f.get_cluster() > max_cluster:
+            max_cluster = f.get_cluster()
+            max_offset = fs.get_data_cluster_address(max_cluster) + f.get_size()
+    return max_offset
 
 
-class FATHandler(StructHandler):
+def no_op():
+    return
+
+
+class FATHandler(Handler):
     NAME = "fat"
 
     PATTERNS = [
@@ -33,127 +51,25 @@ class FATHandler(StructHandler):
         )
     ]
 
-    C_DEFINITIONS = r"""
-        // Common between FAT12, FAT16 and FAT32.
-        typedef struct bios_param_common {
-            char JmpBoot[3];     // An x86 JMP - e.g. EB 3C 90 ("One finds either eb xx 90, or e9 xx xx.")
-            char OEMName[8];     // OEM name/version (E.g. "IBM  3.3", "IBM 20.0", "MSDOS5.0", "MSWIN4.0".)
-            // "BIOS Parameter Block" starts here
-            uint16 BytesPerSec; // Almost always 512? Microsoft operating systems
-                                    // will properly support 1024, 2048, and 4096.
-            uint8 SecPerClus;   // The legal values are 1, 2, 4, 8, 16, 32, 64,
-                                    // and 128.
-            uint16 RsvdSecCnt;  // Must not be 0. Should be 1 on FAT12/16? 32 on FAT32?
-            uint8 NumFATs;      // Should always be 2?
-            uint16 RootEntCnt;  // Number of 32-byte dir entries. Must be 0 for FAT32.
-            uint16 TotSectors;    // Old 16-bit count of all sectors on volume
-                                    // This field can be 0; if it is 0, then TotSec32 must be
-                                    // non-zero. For FAT32, this field must be 0. For FAT12 and
-                                    // FAT16 volumes, this field contains the sector count, and
-                                    // TotSec32 is 0 if the total sector count fits (is less than
-                                    // 0x10000).
-            uint8 Media;        // The valid values for this field is 0xF0, 0xF8, 0xF9, 0xFA, 0xFB,
-                                    // 0xFC, 0xFD, 0xFE and 0xFF.
-            uint16 FATSz16;     // This field is the FAT12/FAT16 16-bit count of sectors occupied by
-                                    // ONE FAT. On FAT32 volumes this field must be 0, and
-                                    // FATSz32 contains the FAT size count.
-            uint16 SecPerTrk;   // Sectors per track for interrupt 0x13.
-            uint16 NumHeads;    // Number of heads for interrupt 0x13
-        } bios_param_common_t;
-
-        // BIOS params for FAT16.
-        typedef struct fat12_16_bootsec {
-            bios_param_common_t common;
-            uint32 NumHidden;
-            uint32 NumSectors;
-            uint8 DrvNum;
-            uint8 Reserved1;
-            uint8 BootSig; // If it's 0x29 (or 0x28 on NT), means that the next 3 fields are present
-            char VolID[4];
-            char VolLab[11];
-            char FileSysType[8]; // Filesystem type (E.g. "FAT12   ", "FAT16   ", "FAT     ", or all zero.)
-        } fat12_16_bootsec_t;
-
-        // BIOS params for FAT32.
-        typedef struct fat32_bootsec {
-            bios_param_common_t common;
-            uint32 Num_Hidden;
-            uint32 TotSec32;
-            uint32 FATSz32;
-            uint16 ExtFlags;
-            uint16 FSVer;
-            uint32 RootClus;
-            uint16 FSInfo;
-            uint16 BkBootSec;
-            uint8 Reserved[12];
-            uint8 DrvNum;
-            uint8 Reserved1;
-            uint8 BootSig;
-            char VolID[4];
-            char VolLab[11];
-            char FileSysType[8];
-        } fat32_bootsec_t;
-
-        typedef struct fat_unknown {
-            bios_param_common_t common;
-        } fat_unknown_t;
-    """
-
     EXTRACTOR = Command("7z", "x", "-y", "{inpath}", "-o{outdir}")
 
-    def valid_name(self, name: bytes) -> bool:
-        try:
-            name.decode("utf-8")
-        except UnicodeDecodeError:
-            return False
-        return True
-
-    def valid_fat32_header(self, header: Instance) -> bool:
-        if header.common.RootEntCnt != 0:
-            return False
-        if header.common.TotSectors != 0:
-            return False
-        if header.common.FATSz16 != 0:
-            return False
-        return True
-
-    def valid_header(self, header: Instance) -> bool:
-        if not self.valid_name(header.common.OEMName):
-            return False
-        if header.common.BytesPerSec % 2 != 0:
-            return False
-        if header.common.RsvdSecCnt == 0:
-            return False
-        if header.common.Media not in VALID_MEDIAS:
-            return False
-        if header.BootSig not in VALID_NT_SIGNATURES:
-            return False
-        if header.FileSysType not in (b"FAT12   ", b"FAT16   "):
-            return self.valid_fat32_header(header)
-        return True
-
     def calculate_chunk(self, file: File, start_offset: int) -> Optional[ValidChunk]:
-        header = self.cparser_le.fat12_16_bootsec_t(file)
+        pyfat_fs = PyFat()
+        pyfat_fs.close = no_op
+        try:
+            pyfat_fs.set_fp(file)  # type: ignore
+        except PyFATException as e:
+            raise InvalidInputFormat from e
 
-        if header.FileSysType in (b"FAT12   ", b"FAT16   "):
-            if header.common.TotSectors == 0:
-                sector_count = header.NumSectors
-            else:
-                sector_count = header.common.TotSectors
+        if pyfat_fs.bpb_header["BPB_TotSec16"] != 0:
+            total_sectors = pyfat_fs.bpb_header["BPB_TotSec16"]
         else:
-            file.seek(start_offset)
-            header = self.cparser_le.fat32_bootsec_t(file)
-            sector_count = header.TotSec32
+            total_sectors = pyfat_fs.bpb_header["BPB_TotSec32"]
 
-        if not self.valid_header(header):
-            raise InvalidInputFormat("Invalid FAT header.")
-
-        logger.debug("FAT header parsed", header=header, _verbosity=3)
-
-        if sector_count == 0x0:
-            sector_count = header.common.TotSectors
-
-        size = header.common.BytesPerSec * sector_count
+        size = total_sectors * pyfat_fs.bpb_header["BPB_BytsPerSec"]
+        file_size = file.size()
+        if start_offset + size > file_size:
+            size = get_max_offset(pyfat_fs, pyfat_fs.root_dir)
 
         return ValidChunk(
             start_offset=start_offset,
