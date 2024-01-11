e2763efe6169a1f79c33a3ed1844a6b552e5de59
Quentin Kaiser <quentin.kaiser@onekey.com>
2024-01-11 16:06:47 +0100

    fix: raise SandboxError exception if unsupported platform

--- src/sandbox/unsupported.rs
+++ src/sandbox/unsupported.rs
@@ -1,9 +1,5 @@
-use log;
-
 use crate::sandbox::AccessFS;
 
 pub fn restrict_access(_access_rules: &[AccessFS]) -> Result<(), Box<dyn std::error::Error>> {
-    log::warn!("Sandboxing FS access is unavailable on this system");
-
-    Ok(())
+    Err("Sandboxing FS access is unavailable on this system")?
 }
