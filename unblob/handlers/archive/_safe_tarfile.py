e734195047bd2ea7cf370cb9be76bcc646de89f8
Andrew Fasano <fasano@mit.edu>
2024-02-12 18:16:46 -0500

    Bugfix: Check symlink safety relative to link name in tarfile extraction

--- unblob/handlers/archive/_safe_tarfile.py
+++ unblob/handlers/archive/_safe_tarfile.py
@@ -83,9 +83,10 @@ class SafeTarFile:
                     "Converted to extraction relative path.",
                 )
                 tarinfo.linkname = f"./{tarinfo.linkname}"
+
             if not is_safe_path(
                 basedir=extract_root,
-                path=extract_root / tarinfo.linkname,
+                path=extract_root / Path(tarinfo.name).parent / tarinfo.linkname,
             ):
                 self.record_problem(
                     tarinfo,
