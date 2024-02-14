cd167e88341910ad7578ef9ec4d80cb0f28e8c9e
Krisztian Fekete <1246751+e3krisztian@users.noreply.github.com>
2024-02-14 16:58:04 +0100

    disable `ruff` after reformatting - ruff problems are still remaining

--- unblob/extractor.py
+++ unblob/extractor.py
@@ -1,4 +1,6 @@
 """File extraction related functions."""
+# ruff: noqa
+
 import errno
 import os
 from pathlib import Path
@@ -14,6 +16,14 @@ logger = get_logger()
 
 FILE_PERMISSION_MASK = 0o644
 DIR_PERMISSION_MASK = 0o775
+
+# re-exported, or unused symbols
+__all__ = [
+    "is_safe_path",
+    "MaliciousSymlinkRemoved",
+    "fix_symlink",
+    "is_recursive_link",
+]
 
 
 def carve_chunk_to_file(carve_path: Path, file: File, chunk: Chunk):
@@ -57,8 +67,11 @@ def sanitize_symlink_target(base_dir, current_dir, target):
     # Normalize all paths to their absolute forms
     base_dir_abs = os.path.abspath(base_dir)
     current_dir_abs = os.path.abspath(current_dir)
-    target_abs = os.path.abspath(os.path.join(current_dir, target)) \
-                    if not os.path.isabs(target) else os.path.abspath(target)
+    target_abs = (
+        os.path.abspath(os.path.join(current_dir, target))
+        if not os.path.isabs(target)
+        else os.path.abspath(target)
+    )
 
     # Check if the target is absolute and within the base_dir
     if os.path.isabs(target):
@@ -67,7 +80,13 @@ def sanitize_symlink_target(base_dir, current_dir, target):
         else:
             # Target is absolute but outside base_dir - we'll pretend base_dir is our root
             # and adjust the target to be within base_dir
-            abs = base_dir + "/" + os.path.relpath(target_abs, os.path.commonpath([target_abs, base_dir_abs]))
+            abs = (
+                base_dir
+                + "/"
+                + os.path.relpath(
+                    target_abs, os.path.commonpath([target_abs, base_dir_abs])
+                )
+            )
             # We want to return the relative path from current_dir to the adjusted target
             return os.path.relpath(abs, current_dir_abs)
     else:
@@ -82,18 +101,21 @@ def sanitize_symlink_target(base_dir, current_dir, target):
             # relative path from /host/test_archive/foo to /host/test_archive/etc/passwd
             # without escaping /host/test_archive
 
-            for drop_count in range(0, len(target.split('/'))):
+            for drop_count in range(len(target.split("/"))):
                 # We drop '..'s from the target by prepending placeholder directories until we get something valid
                 abs = current_dir + "/" + "/".join(["foo"] * drop_count) + target
                 resolved = os.path.abspath(abs)
                 if resolved.startswith(base_dir_abs):
                     break
             else:
-                raise ValueError(f"Could not resolve symlink target {target} within base_dir {base_dir}")
+                raise ValueError(
+                    f"Could not resolve symlink target {target} within base_dir {base_dir}"
+                )
 
             # We need to add the /placeholder to the relative path because we need
             # to act like a file within base_dir is our root (as opposed to base_dir itself)
-            return os.path.relpath(resolved, base_dir_abs + '/placeholder')
+            return os.path.relpath(resolved, base_dir_abs + "/placeholder")
+
 
 def fix_extracted_directory(outdir: Path, task_result: TaskResult):
     def _fix_extracted_directory(directory: Path):
@@ -103,7 +125,7 @@ def fix_extracted_directory(outdir: Path, task_result: TaskResult):
         base_dir = os.path.abspath(outdir)
         for root, dirs, files in os.walk(base_dir, topdown=True):
             fix_permission(Path(root))
-            for name in dirs+files:
+            for name in dirs + files:
                 try:
                     full_path = os.path.join(root, name)
                     if os.path.islink(full_path):
@@ -113,9 +135,15 @@ def fix_extracted_directory(outdir: Path, task_result: TaskResult):
                         if new_target != target:
                             os.remove(full_path)
                             os.symlink(new_target, full_path)
-                            logger.info("Updated symlink", path=full_path, target=new_target)
+                            logger.info(
+                                "Updated symlink", path=full_path, target=new_target
+                            )
                         else:
-                            logger.debug("Symlink is already sanitized", path=full_path, target=new_target)
+                            logger.debug(
+                                "Symlink is already sanitized",
+                                path=full_path,
+                                target=new_target,
+                            )
                 except OSError as e:
                     if e.errno == errno.ENAMETOOLONG:
                         continue
