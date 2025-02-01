d79903b528d8cbcce8e7d64362d6b65f537aa656
László Vaskó <1771332+vlaci@users.noreply.github.com>
2025-02-01 16:34:29 +0100

    chore: retarget imports to point inside unblob module

--- unblob/processing.py
+++ unblob/processing.py
@@ -9,8 +9,8 @@ import attrs
 import magic
 import plotext as plt
 from structlog import get_logger
-from unblob_native import math_tools as mt
 
+from unblob._rust import math_tools as mt
 from unblob.handlers import BUILTIN_DIR_HANDLERS, BUILTIN_HANDLERS, Handlers
 
 from .extractor import carve_unknown_chunk, carve_valid_chunk, fix_extracted_directory
