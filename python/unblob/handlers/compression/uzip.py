61ff4aba439421f34e8a8568d3aee4697eff2d86
Raphael Cremer <r.cremer@student.helmo.be>
2025-03-03 16:21:22 +0100

    feat(handler): add geom handler for uzip, lzma and zstd compression
    Geom_uzip is a FreeBSD feature for creating compressed disk images
    (usually containing UFS). The compression is done in blocks, and
    the resulting .uzip file can be mounted via the GEOM framework on
    FreeBSD.
    
    The mkuzip header includes a table with block counts and sizes.
    The header declares the block size (size of decompressed blocks)
    and total number of blocks. Block size must be a multiple of 512
    and defaults to 16384 in mkuzip.
    It has the following structure:
    > Magic, which is a shebang & compression identifier stored on 16 bytes.
    > Format, which is a shell command that provides some general information.
    > Block size, stored on 4 bytes.
    > Block count, stored on 4 bytes.
    > Table of content (TOC), which depends on the file lentgh.
    The TOC is a list of uint64_t offsets into the file for each block.
    To determine the length of a given block, read the next TOC entry
    and subtract the current offset from the next offset (this is why
    there is an extra TOC entry at the end). Each block is compressed
    using zlib. A standard zlib decompressor will decode them to a block
    of size block_size.
    
    Unblob parses the TOC to determine end & start offset of the compressed
    file. It detects the compression method (zlib, lzma or zstd). Finally
    the chunks are decompressed to revocer the inital file. Empty chunks are
    ignored, which is why the decompressed file with unlbob can be a little
    bit lighter than the original one.
    
    [Sources]
    https://github.com/mikeryan/unuzip
    https://www.baeldung.com/linux/filesystem-in-a-file
    https://docs.python.org/3/library/zlib.html
    https://github.com/freebsd/freebsd-src/blob/master/sys/geom/uzip/g_uzip.c
    https://parchive.sourceforge.net/docs/specifications/parity-volume-spec/article-spec.html
    https://www.mail-archive.com/dev-commits-src-main@freebsd.org/msg34955.html

---
+++ python/unblob/handlers/compression/uzip.py
@@ -0,0 +1,133 @@
+import lzma
+import re
+import zlib
+from pathlib import Path
+from typing import Callable, Optional
+
+import pyzstd
+
+from unblob.file_utils import (
+    Endian,
+    FileSystem,
+    InvalidInputFormat,
+    StructParser,
+    iterate_file,
+)
+from unblob.models import (
+    Extractor,
+    ExtractResult,
+    File,
+    Regex,
+    StructHandler,
+    ValidChunk,
+)
+
+# [Ref] https://github.com/freebsd/freebsd-src/tree/master/sys/geom/uzip
+C_DEFINITIONS = r"""
+    typedef struct uzip_header{
+        char magic[16];
+        char format[112];
+        uint32_t block_size;
+        uint32_t block_count;
+        uint64_t toc[block_count];
+    } uzip_header_t;
+"""
+
+HEADER_STRUCT = "uzip_header_t"
+
+ZLIB_COMPRESSION = "#!/bin/sh\x0a#V2.0\x20"
+LZMA_COMPRESSION = "#!/bin/sh\x0a#L3.0\x0a"
+ZSTD_COMPRESSION = "#!/bin/sh\x0a#Z4.0\x20"
+
+
+class Decompressor:
+    DECOMPRESSOR: Callable
+
+    def __init__(self):
+        self._decompressor = self.DECOMPRESSOR()
+
+    def decompress(self, data: bytes) -> bytes:
+        return self._decompressor.decompress(data)
+
+    def flush(self) -> bytes:
+        return b""
+
+
+class LZMADecompressor(Decompressor):
+    DECOMPRESSOR = lzma.LZMADecompressor
+
+
+class ZLIBDecompressor(Decompressor):
+    DECOMPRESSOR = zlib.decompressobj
+
+    def flush(self) -> bytes:
+        return self._decompressor.flush()
+
+
+class ZSTDDecompressor(Decompressor):
+    DECOMPRESSOR = pyzstd.EndlessZstdDecompressor
+
+
+DECOMPRESS_METHOD: dict[bytes, type[Decompressor]] = {
+    ZLIB_COMPRESSION.encode(): ZLIBDecompressor,
+    LZMA_COMPRESSION.encode(): LZMADecompressor,
+    ZSTD_COMPRESSION.encode(): ZSTDDecompressor,
+}
+
+
+class UZIPExtractor(Extractor):
+    def extract(self, inpath: Path, outdir: Path):
+        with File.from_path(inpath) as infile:
+            parser = StructParser(C_DEFINITIONS)
+            header = parser.parse(HEADER_STRUCT, infile, Endian.BIG)
+            fs = FileSystem(outdir)
+            outpath = Path(inpath.stem)
+
+            try:
+                decompressor_cls = DECOMPRESS_METHOD[header.magic]
+            except LookupError:
+                raise InvalidInputFormat("unsupported compression format") from None
+
+            with fs.open(outpath, "wb+") as outfile:
+                for current_offset, next_offset in zip(header.toc[:-1], header.toc[1:]):
+                    compressed_len = next_offset - current_offset
+                    if compressed_len == 0:
+                        continue
+                    decompressor = decompressor_cls()
+                    for chunk in iterate_file(infile, current_offset, compressed_len):
+                        outfile.write(decompressor.decompress(chunk))
+                    outfile.write(decompressor.flush())
+            return ExtractResult(reports=fs.problems)
+
+
+class UZIPHandler(StructHandler):
+    NAME = "uzip"
+    PATTERNS = [
+        Regex(re.escape(ZLIB_COMPRESSION)),
+        Regex(re.escape(LZMA_COMPRESSION)),
+        Regex(re.escape(ZSTD_COMPRESSION)),
+    ]
+    HEADER_STRUCT = HEADER_STRUCT
+    C_DEFINITIONS = C_DEFINITIONS
+    EXTRACTOR = UZIPExtractor()
+
+    def is_valid_header(self, header) -> bool:
+        return (
+            header.block_count > 0
+            and header.block_size > 0
+            and header.block_size % 512 == 0
+        )
+
+    def calculate_chunk(self, file: File, start_offset: int) -> Optional[ValidChunk]:
+        header = self.parse_header(file, Endian.BIG)
+
+        if not self.is_valid_header(header):
+            raise InvalidInputFormat("Invalid uzip header.")
+
+        # take the last TOC block offset, end of file is that block offset,
+        # starting from the start offset
+        end_offset = start_offset + header.toc[-1]
+        return ValidChunk(
+            start_offset=start_offset,
+            end_offset=end_offset,
+        )
