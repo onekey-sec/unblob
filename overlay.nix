03f5bccc4a2d7bc451cd4717845e36b8346c5de5
János Kukovecz <janos.kukovecz@onekey.com>
2025-04-11 15:43:03 +0200

    chore(deps): Downgrade 7zip to v16.02 in nix packaging
    Found some problems with 7z based extractions using the nix environment
    which does not happen in debian. The reason is that debian still using a
    quite old stable version of the 7z, while a newer one is coming from
    nixpkgs.
    
    This commit downgrades 7zip version used by unblob's nix packaging to the same
    version (applying the same patches) as debian does, hopefully making the
    extractors more stable using nix-based environment.

--- overlay.nix
+++ overlay.nix
@@ -8,5 +8,26 @@ final: prev:
     nativeCheckInputs = (super.nativeCheckInputs or [ ]) ++ [ final.which ];
   });
 
+  p7zip16 = prev.p7zip.overrideAttrs (super: rec {
+    pname = "p7zip16";
+    version = "16.02";
+    srcs = [
+      (final.fetchurl {
+        url = "mirror://sourceforge/p7zip/p7zip_${version}_src_all.tar.bz2";
+        sha256 = "5eb20ac0e2944f6cb9c2d51dd6c4518941c185347d4089ea89087ffdd6e2341f";
+      })
+      (final.fetchurl {
+        url = "http://deb.debian.org/debian/pool/main/p/p7zip/p7zip_${version}+dfsg-8.debian.tar.xz";
+        sha256 = "sha256-ASF9yhZnrw3kiTWlHcRqrUQubryseZ1xQQG37fllHrU=";
+      })
+    ];
+    sourceRoot = "p7zip_${version}";
+    nativeBuildInputs = (super.nativeBuildInputs or [ ]) ++ [ final.quilt ];
+    prePatch = ''
+      export QUILT_PATCHES=../debian/patches
+      quilt push -a
+    '';
+  });
+
   unblob = final.callPackage ./package.nix { };
 }
