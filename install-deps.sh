05b92b87d1b720b89050638a5ab82caa4b9ea633
Quentin Kaiser <quentin.kaiser@onekey.com>
2025-09-02 11:50:30 +0200

    chore(deps): replace e2fsprogs source from fork to upstream
    Our debugfs fix¹ is present in the debian trixie version² and in latest
    Ubuntu³. We can therefore go back to installing it from upstream rather
    than relying on our own fork.
    
    ¹:
    https://github.com/tytso/e2fsprogs/commit/56a444e898be418afd4bd11f2a6950dce7e4ae99
    ²:
    https://metadata.ftp-master.debian.org/changelogs//main/e/e2fsprogs/e2fsprogs_1.47.2-3_changelog
    ³: https://launchpad.net/ubuntu/+source/e2fsprogs

--- install-deps.sh
+++ install-deps.sh
@@ -5,6 +5,7 @@ apt-get update
 apt-get install --no-install-recommends -y \
     android-sdk-libsparse-utils \
     curl \
+    e2fsprogs \
     erofs-utils \
     lz4 \
     lziprecover \
@@ -20,10 +21,3 @@ apt-get install --no-install-recommends -y \
 curl -L -o sasquatch_1.0.deb "https://github.com/onekey-sec/sasquatch/releases/download/sasquatch-v4.5.1-6/sasquatch_1.0_$(dpkg --print-architecture).deb"
 dpkg -i sasquatch_1.0.deb
 rm -f sasquatch_1.0.deb
-
-curl -L -o libext2fs2_1.47.0-3.ok2.deb "https://github.com/onekey-sec/e2fsprogs/releases/download/v1.47.0-3.ok2/libext2fs2_1.47.0-3.ok2_$(dpkg --print-architecture).deb"
-curl -L -o e2fsprogs_1.47.0-3.ok2.deb "https://github.com/onekey-sec/e2fsprogs/releases/download/v1.47.0-3.ok2/e2fsprogs_1.47.0-3.ok2_$(dpkg --print-architecture).deb"
-curl -L -o libss2_1.47.0-3.ok2.deb "https://github.com/onekey-sec/e2fsprogs/releases/download/v1.47.0-3.ok2/libss2_1.47.0-3.ok2_$(dpkg --print-architecture).deb"
-dpkg -i libext2fs2_1.47.0-3.ok2.deb libss2_1.47.0-3.ok2.deb
-dpkg -i e2fsprogs_1.47.0-3.ok2.deb
-rm -f libext2fs2_1.47.0-3.ok2.deb libss2_1.47.0-3.ok2.deb e2fsprogs_1.47.0-3.ok2.deb
