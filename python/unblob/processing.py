551bc49a950944c56c22374feaa3b0c4b5b09687
Jon Crussell <jcrusse@sandia.gov>
2025-11-28 21:07:20 -0800

    feat(handler): add support for MSI files
    Seems to work but had to DIY the parsing due to issues with olefile.

--- python/unblob/processing.py
+++ python/unblob/processing.py
@@ -54,7 +54,8 @@ DEFAULT_DEPTH = 10
 DEFAULT_PROCESS_NUM = multiprocessing.cpu_count()
 DEFAULT_SKIP_MAGIC = (
     "BFLT",
-    "Composite Document File V2 Document",
+    # Disabled for MSI files
+    #"Composite Document File V2 Document",
     "Erlang BEAM file",
     "GIF",
     "GNU message catalog",
