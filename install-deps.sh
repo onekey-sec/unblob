54e4550fb26fd6915e0e610a71dc409b0ab442af
Quentin Kaiser <quentin.kaiser@onekey.com>
2026-01-27 13:48:44 +0100

    chore(deps): upgrade sasquatch to 4.5.1-6.

--- install-deps.sh
+++ install-deps.sh
@@ -17,7 +17,7 @@ apt-get install --no-install-recommends -y \
     libmagic1 \
     zstd
 
-curl -L -o sasquatch_1.0.deb "https://github.com/onekey-sec/sasquatch/releases/download/sasquatch-v4.5.1-5/sasquatch_1.0_$(dpkg --print-architecture).deb"
+curl -L -o sasquatch_1.0.deb "https://github.com/onekey-sec/sasquatch/releases/download/sasquatch-v4.5.1-6/sasquatch_1.0_$(dpkg --print-architecture).deb"
 dpkg -i sasquatch_1.0.deb
 rm -f sasquatch_1.0.deb
 
