ee453f23fc07892bbee4916d82e5637eb164f77b
László Vaskó <opensource@accounts.vlaci.email>
2026-03-27 22:46:07 +0100

    fix(tests): only skip actual end-to-en-tests
    `pytest_collection_modifyitems` affect all tests, not only those i the
    current directory.

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -1,7 +1,12 @@
+from pathlib import Path
+
 import pytest
+
+E2E_DIR = Path(__file__).parent
 
 
 def pytest_collection_modifyitems(items):
     # imply e2e to all tests under this directory
     for item in items:
-        item.add_marker(pytest.mark.e2e)
+        if Path(item.fspath).is_relative_to(E2E_DIR):
+            item.add_marker(pytest.mark.e2e)
