074ba1c5c18ac2eabaa4f17616d192a2de023fd9
elektrischermoench <git@koffeinsucht.de>
2026-04-24 22:21:02 +0200

    feat(handler): add APFS container handler

--- python/unblob/handlers/__init__.py
+++ python/unblob/handlers/__init__.py
@@ -46,6 +46,7 @@ from .compression import (
 )
 from .executable import elf, xalz
 from .filesystem import (
+    apfs,
     btrfs_stream,
     cramfs,
     extfs,
@@ -71,6 +72,7 @@ __all__ = [
 ]
 
 BUILTIN_HANDLERS: Handlers = (
+    apfs.APFSHandler,
     cramfs.CramFSHandler,
     deafbead.DeafBeadHandler,
     extfs.EXTHandler,
